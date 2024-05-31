30b19d3ea40b8fcaa7330154c45cf0f514e9ee5b
kirari04 <lev@saminskij.ch>
2024-05-31 20:28:15 +0200

    switching db driver and adding settings migration

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,9 +3,9 @@ package db
 import (
 	"time"
 
-	"github.com/glebarez/sqlite"
 	"github.com/go-gormigrate/gormigrate/v2"
 	"github.com/rs/zerolog/log"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -68,6 +68,54 @@ func Connect() (*gorm.DB, error) {
 					FrontendID uint `gorm:"index,column:frontend_id"`
 				}
 				return db.Migrator().DropTable(&Backend{})
+			},
+		},
+		{
+			ID: "3",
+			Migrate: func(tx *gorm.DB) error {
+				type Setting struct {
+					ID        uint      `gorm:"primaryKey;column:id" json:"id"`
+					CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+					UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
+
+					ShouldProxyRun bool `gorm:"column:should_proxy_run" json:"should_proxy_run"`
+				}
+				return tx.Migrator().CreateTable(&Setting{})
+			},
+			Rollback: func(tx *gorm.DB) error {
+				type Setting struct {
+					ID        uint      `gorm:"primaryKey;column:id" json:"id"`
+					CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+					UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
+
+					ShouldProxyRun bool `gorm:"column:should_proxy_run" json:"should_proxy_run"`
+				}
+				return db.Migrator().DropTable(&Setting{})
+			},
+		},
+		{
+			ID: "4",
+			Migrate: func(tx *gorm.DB) error {
+				type Setting struct {
+					ID        uint      `gorm:"primaryKey;column:id" json:"id"`
+					CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+					UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
+
+					ShouldProxyRun bool `gorm:"column:should_proxy_run" json:"should_proxy_run"`
+				}
+				return tx.Create(&Setting{
+					ShouldProxyRun: false,
+				}).Error
+			},
+			Rollback: func(tx *gorm.DB) error {
+				type Setting struct {
+					ID        uint      `gorm:"primaryKey;column:id" json:"id"`
+					CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+					UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
+
+					ShouldProxyRun bool `gorm:"column:should_proxy_run" json:"should_proxy_run"`
+				}
+				return tx.Where("1 = 1").Delete(&Setting{}).Error
 			},
 		},
 	})
