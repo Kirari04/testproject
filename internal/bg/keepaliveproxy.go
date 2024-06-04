18419657550aa11010ed88cc156b1c44daa858e5
kirari04 <lev@saminskij.ch>
2024-06-05 00:02:10 +0200

    faster restarts and startup of proxy

--- internal/bg/keepaliveproxy.go
+++ internal/bg/keepaliveproxy.go
@@ -10,27 +10,32 @@ import (
 )
 
 func KeepAliveProxy(s t.Server) {
+	runKeepAliveProxy(s)
 	for {
-		time.Sleep(time.Second * 5)
-		tx := s.DB()
-		var setting m.Setting
-		if err := tx.First(&setting).Error; err != nil {
-			log.Error().Err(err).Msg("failed to get setting inside keepalive")
-			continue
-		}
+		time.Sleep(time.Second * 2)
+		runKeepAliveProxy(s)
+	}
+}
 
-		if app.Proxy.IsRunning() != setting.ShouldProxyRun {
-			log.Info().
-				Bool("IsRunning", app.Proxy.IsRunning()).
-				Bool("ShouldBeRunning", setting.ShouldProxyRun).
-				Msg("keepalive proxy state mismatch")
-			if !setting.ShouldProxyRun {
-				log.Info().Msg("keepalive detected proxy running")
-				app.Proxy.Stop()
-			} else {
-				log.Info().Msg("keepalive detected proxy not running")
-				app.Proxy.Start()
-			}
+func runKeepAliveProxy(s t.Server) {
+	tx := s.DB()
+	var setting m.Setting
+	if err := tx.First(&setting).Error; err != nil {
+		log.Error().Err(err).Msg("failed to get setting inside keepalive")
+		return
+	}
+
+	if app.Proxy.IsRunning() != setting.ShouldProxyRun {
+		log.Info().
+			Bool("IsRunning", app.Proxy.IsRunning()).
+			Bool("ShouldBeRunning", setting.ShouldProxyRun).
+			Msg("keepalive proxy state mismatch")
+		if !setting.ShouldProxyRun {
+			log.Info().Msg("keepalive detected proxy running")
+			app.Proxy.Stop()
+		} else {
+			log.Info().Msg("keepalive detected proxy not running")
+			app.Proxy.Start()
 		}
 	}
 }
